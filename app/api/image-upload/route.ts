5715096be02c7203c143a8747ccd8d19b1f5bb46
Darshan Subedi <subedidarshan02@gmail.com>
2025-02-14 19:37:45 +0530

    Refactor image upload type definition to use 'unknown' for dynamic properties

--- app/api/image-upload/route.ts
+++ app/api/image-upload/route.ts
@@ -12,7 +12,7 @@ cloudinary.config({
 
 interface CloudinaryUploadResult {
   public_id: string;
-  [key: string]: any;
+  [key: string]: unknown;
 }
 
 export async function POST(req: NextRequest) {
